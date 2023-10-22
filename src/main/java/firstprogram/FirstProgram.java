a06b694bc2327ea06d5727eb933950fda57f3ed4
ahmadnurhadi <ahmadnurhadi77@gmail.com>
2023-10-22 12:23:21 +0700

    Initial commit

---
+++ src/main/java/firstprogram/FirstProgram.java
@@ -0,0 +1,11 @@
+
+
+package firstprogram;
+
+public class FirstProgram {
+public static void main(String[] args){
+    System.out.println("Hell no World");
+
+}
+
+}
---
+++ src/main/java/firstprogram/Percabangan.java
@@ -0,0 +1,2 @@
+package firstprogram;public class Percabangan {
+}
